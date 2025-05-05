1cafd27701f55257ec45993f4b43e5dfa2f2f44a
Ruan Barreto <barretoruan65@gmail.com>
2025-05-05 11:56:04 -0300

    Criação dos Metodos

---
+++ src/main/java/com/generation/projeto_final_bloco_02/model/Categoria.java
@@ -0,0 +1,52 @@
+package com.generation.projeto_final_bloco_02.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@Size(min = 3, max = 255)
+	private String nome;
+
+	@NotBlank
+	@Size(min = 3, max = 255)
+    private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+    
+    
+
+}
